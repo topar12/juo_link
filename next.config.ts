0b9849e289b577fdc86410f9a54fb1021ad5c9c6
TotalpetcareJuo <petfoodjuo@gmail.com>
2026-04-08 13:18:57 +0900

    Configure Cloudflare OpenNext deployment

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 
 const nextConfig: NextConfig = {
   /* config options here */
 };
+
+initOpenNextCloudflareForDev();
 
 export default nextConfig;
